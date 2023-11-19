3caff07326cc5a221ab0e9d45cb7a33527a2e25d
liangh80 <hanwen.liang@mail.utoronto.ca>
2023-11-19 18:42:10 -0500

    optimize FileUser DAO and add user.csv file

--- pro/src/data_access/FileUserDataAccessObject.java
+++ pro/src/data_access/FileUserDataAccessObject.java
@@ -1,17 +1,59 @@
 package data_access;
 
 import entity.User;
+import entity.UserFactory;
 import service.delete.use_case.DeleteRecipeDataAccessInterface;
 import service.read.use_case.ReadRecipeDataAccessInterface;
 import service.save.use_case.SaveRecipeDataAccessInterface;
+import service.signup.use_case.SignupUserDataAccessInterface;
 
+import java.io.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class FileUserDataAccessObject implements SaveRecipeDataAccessInterface, DeleteRecipeDataAccessInterface,
+public class FileUserDataAccessObject implements SignupUserDataAccessInterface, SaveRecipeDataAccessInterface, DeleteRecipeDataAccessInterface,
         ReadRecipeDataAccessInterface {
+    private final File csvFile;
+
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
     private final Map<String, User> accounts = new HashMap<>();
+
+    private UserFactory userFactory;
+
+    public FileUserDataAccessObject(String csvPath, UserFactory userFactory) throws IOException {
+        this.userFactory = userFactory;
+
+        csvFile = new File(csvPath);
+        headers.put("username", 0);
+        headers.put("password", 1);
+        headers.put("creation_time", 2);
+
+        if (csvFile.length() == 0) {
+            save();
+        } else {
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String header = reader.readLine();
+
+                // For later: clean this up by creating a new Exception subclass and handling it in the UI.
+                assert header.equals("username,password,creation_time");
+
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    String username = String.valueOf(col[headers.get("username")]);
+                    String password = String.valueOf(col[headers.get("password")]);
+                    String creationTimeText = String.valueOf(col[headers.get("creation_time")]);
+                    LocalDateTime ldt = LocalDateTime.parse(creationTimeText);
+                    User user = userFactory.create(username, password, ldt);
+                    accounts.put(username, user);
+                }
+            }
+        }
+    }
     @Override
     public void saveRecipe(String userName, String recipeName) {
         accounts.get(userName).setFavoriteRecipes(recipeName);
@@ -23,5 +65,37 @@ public class FileUserDataAccessObject implements SaveRecipeDataAccessInterface, 
     @Override
     public void deleteRecipe(String userName, String recipeName) {
         accounts.get(userName).getFavoriteRecipes().remove(recipeName);
+    }
+
+    @Override
+    public boolean existsByName(String identifier) {
+        return accounts.containsKey(identifier);
+    }
+
+    @Override
+    public void save(User user) {
+        accounts.put(user.getName(), user);
+        this.save();
+    }
+
+    private void save() {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(csvFile));
+            writer.write(String.join(",", headers.keySet()));
+            writer.newLine();
+
+            for (User user : accounts.values()) {
+                String line = String.format("%s,%s,%s",
+                        user.getName(), user.getPassword(), user.getCreationTime());
+                writer.write(line);
+                writer.newLine();
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
