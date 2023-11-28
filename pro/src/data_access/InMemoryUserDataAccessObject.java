9a5f8bf1e799524e881b9e9067602ac59f948140
TeresasaZ <teresa.zhao@mail.utoronto.ca>
2023-11-28 17:29:21 -0500

    modified in memory and file user DAO

--- pro/src/data_access/InMemoryUserDataAccessObject.java
+++ pro/src/data_access/InMemoryUserDataAccessObject.java
@@ -2,6 +2,7 @@ package data_access;
 
 import entity.User;
 import service.load_favourite_recipes.use_case.LoadRecipesDataAccessInterface;
+import service.login.use_case.LoginUserDataAccessInterface;
 import service.save_favourite_recipe.use_case.SaveRecipeDataAccessInterface;
 import service.remove_favourite_recipe.use_case.RemoveRecipeDataAccessInterface;
 import service.signup.use_case.SignupUserDataAccessInterface;
@@ -11,7 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class InMemoryUserDataAccessObject implements SaveRecipeDataAccessInterface, RemoveRecipeDataAccessInterface,
-        LoadRecipesDataAccessInterface, SignupUserDataAccessInterface {
+        LoadRecipesDataAccessInterface, SignupUserDataAccessInterface, LoginUserDataAccessInterface {
     private final Map<String, User> users = new HashMap<>();
 
     @Override
@@ -38,5 +39,10 @@ public class InMemoryUserDataAccessObject implements SaveRecipeDataAccessInterfa
     @Override
     public void save(User user) {
         users.put(user.getName(), user);
+    }
+
+    @Override
+    public User get(String username) {
+        return users.get(username);
     }
 }
