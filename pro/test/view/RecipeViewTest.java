484d5236a571b7a75257cd7bbb6296c368b4e303
TeresasaZ <teresa.zhao@mail.utoronto.ca>
2023-12-02 17:36:49 -0500

    login view and recipe view test added

---
+++ pro/test/view/RecipeViewTest.java
@@ -0,0 +1,85 @@
+package view;
+
+import app.Main;
+import app.SearchUseCaseFactory;
+import data_access.FileUserDataAccessObject;
+import entity.CommonUserFactory;
+import entity.UserFactory;
+import service.search.interface_adapter.SearchViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertNotNull;
+
+public class RecipeViewTest {
+
+    public JButton getButton() {
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+
+        assertNotNull(app); // found the window?
+
+        Component root = app.getComponent(0);
+
+        Component cp = ((JRootPane) root).getContentPane();
+
+        JPanel jp = (JPanel) cp;
+
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+
+        SearchView sv = (SearchView) jp2.getComponent(2);
+
+        JPanel buttons = (JPanel) sv.getComponent(2);
+
+        return (JButton) buttons.getComponent(0);
+    }
+    @org.junit.Test
+    public void testSignupButtonPresent() throws IOException {
+        Main.main(null);
+        JButton button = getButton();
+
+        assert(button.getText().equals("Search"));
+    }
+    public JButton getButton2() {
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+
+        assertNotNull(app); // found the window?
+
+        Component root = app.getComponent(0);
+
+        Component cp = ((JRootPane) root).getContentPane();
+
+        JPanel jp = (JPanel) cp;
+
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+
+        SearchResultView sv = (SearchResultView) jp2.getComponent(3);
+
+        JPanel buttons = (JPanel) sv.getComponent(1);
+
+        return (JButton) buttons.getComponent(0);
+    }
+    @org.junit.Test
+    public void testSearchResultMatch() throws IOException {
+        Main.main(null);
+        JButton button = getButton();
+        button.doClick();
+        JButton button2 = getButton2();
+        System.out.println(button2.getText());
+        assert(button2.getText().equals("Corba"));
+    }
+}
